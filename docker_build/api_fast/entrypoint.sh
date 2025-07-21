070c5ccb1c98640e802866445ccc91e5aebce010
Xiedexiao <xiedex@smjPC.localdomain>
2025-07-21 22:25:09 +0800

    修复fastapi服务

--- docker_build/api_fast/entrypoint.sh
+++ docker_build/api_fast/entrypoint.sh
@@ -7,14 +7,14 @@ export MINERU_MODEL_SOURCE=local
 env=${START_TYPE}
 
 if [ "$env" = "sglang" ]; then
-    echo "当前环境：开发环境"
+    echo "当前环境：开发环境, 启动sglang-server"
+    nohup uvicorn api_fast.server_fast:app --host 0.0.0.0 --port 8000 > logs/fastapi.log 2>&1 &
     mineru-sglang-server --host 0.0.0.0 --port 30000
-    uvicorn fastapi_server:app --host 0.0.0.0 --port 8000
 elif [ "$env" = "api" ]; then
-    echo "当前环境：测试环境"
+    echo "当前环境：测试环境，启动api服务"
     mineru-api --host 0.0.0.0 --port 8000
 elif [ "$env" = "gradio" ]; then
-    echo "当前环境：生产环境"
+    echo "当前环境：生产环境，启动gradio服务"
     mineru-gradio --host 0.0.0.0 --port 8000 --enable-sglang-engine true
 else
     echo "未知环境变量: $env"
